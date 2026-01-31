be60d0ae3348e505506ce8abe6030f2fc4850cc2
Rouven Blättler <rouvenblattler@gmail.com>
2026-01-31 20:03:33 +0100

    create login, logout and register view

--- game/views.py
+++ game/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Game, Player
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'games/register.html', {'form': form})
+        
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username = username, password = password)
+        if user:
+            login(request, user)
+            return redirect('game/gamelobby.html')
+
+        else:
+            return render(request, 'game/login.html', {'error': 'incorrect password or username' })
+        
+    return render(request, 'game/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('game/login.html')
+
+    
+
+
 
 @login_required
 def game_lobby(request):
@@ -8,9 +46,12 @@ def game_lobby(request):
     context = {"available_games": available_games}
     return render(request, "game/gamelobby.html", context)
 
-
+@login_required
 def game_view(request, game_id):
-    game = get_object_or_404(Game, id = game_id, player = request.player)
+    game = get_object_or_404(Game, id=game_id)
+    player = get_object_or_404(Player, user=request.user, game=game)
+    context = {"game": game, "player": player}
+    return render(request, "game/game.html", context)
 
 @login_required
 def join_game(request, game_id):
