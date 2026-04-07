124a6ab8d7cf05b13c8832022c1d21673b0e7707
iFarmApples <rouvenblattler@gmail.com>
2026-04-07 09:42:13 +0200

    add comments to wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
-import os
+import os      #not being used because daphne uses an ASGI entrypoint
 
 from django.core.wsgi import get_wsgi_application
 
